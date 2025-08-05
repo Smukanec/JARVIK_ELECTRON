eb0ca74f4b769a91ea7075a8d5e770754dd46b53
J.A.R.V.I.K <jiri.cechura@gmail.com>
2025-08-05 10:11:44 +0200

    feat: add start script for Windows

---
+++ start.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+set API_KEY=7d1fd70c60ac827d2bddc4de879804e8
+set USERNAME=jiri.cechura
+set PASSWORD_HASH=$2b$12$4NuMNFox0FS0Q7BIMV4gpOR7x/7k3wm9urq1j8HrR01Y.HnAx9l3u
+set EMAIL=jiri.cechura@gmail.com
+set APPROVED=true
+set CREATED_AT=2025-08-04T10:18:45.759182
+
+REM Install dependencies if needed
+npm install
+
+REM Launch the Electron app
+npm start
+
+pause
